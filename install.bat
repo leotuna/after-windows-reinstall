f3d38e18cb3392d80efc77e808a11d0f83fd9572
Leonardo Tuna <tunaleo@gmail.com>
2021-10-16 22:55:17 -0300

    init

---
+++ install.bat
@@ -0,0 +1,22 @@
+:: Chocolatey install script from earlier
+
+@powershell -NoProfile -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
+
+
+:: Install all the packages
+:: -y confirm yes for any prompt during the install process ﻿
+
+choco install adobereader -fy 
+choco install git -fy 
+choco install GoogleChrome -fy
+choco install firefox -fy
+choco install googledrive -fy 
+choco install nodejs -fy-lts
+choco install postman -fy 
+choco install python -fy 
+choco install spotify -fy 
+choco install vlc -fy 
+choco install vscode -fy 
+choco install winrar -fy 
+choco install yarn  
+choco install microsoft-windows-terminal -fy
