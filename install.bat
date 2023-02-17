a04b246368f46135c704fa109cd30e69a322e26b
Leonardo Tuna <tunaleo@gmail.com>
2023-02-17 14:52:48 -0300

    commit

--- install.bat
+++ install.bat
@@ -1,19 +1,16 @@
 @powershell -NoProfile -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
 
-choco install adobereader -fy 
 choco install git -fy 
 choco install GoogleChrome -fy
-choco install firefox -fy
 choco install googledrive -fy 
-choco install nodejs-lts -fy
+choco install nvm -fy
 choco install postman -fy 
 choco install python -fy 
 choco install spotify -fy 
-choco install vlc -fy 
 choco install vscode -fy 
 choco install winrar -fy 
 choco install yarn -fy
 choco install microsoft-windows-terminal -fy
 choco install docker-desktop -fy
-choco install dotnet-sdk -fy
-choco install insomnia-rest-api-client -fy
+choco install mailspring -fy
+choco install googledrive -fy
